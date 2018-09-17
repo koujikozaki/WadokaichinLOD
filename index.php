eeb82635150b06a44ae6ab83520a14b47d3bf623
Kouji Kozaki <kozaki@ei.sanken.osaka-u.ac.jp>
2018-09-17 14:05:23 +0900

    p034

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php 
 
-echo "Hello World1";
+//echo "Hello World1";
+
+require_once __DIR__ . 'vendor/autoload.php';
+
+$inputString = file_getcontents('php://input');
+error_log($inputString);
 
 ?>
